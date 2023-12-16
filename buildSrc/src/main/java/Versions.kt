155f958e1c731f306eb4bd602564595bb8fde7b2
Mohammad <mohammadazadbar1373@gmail.com>
2023-12-16 11:16:29 +0330

    [feature] add Dependencies and buildSrc

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,12 @@
+object Versions {
+
+    val coreKtx = "1.12.0"
+    val runtimeKtx = "2.6.2"
+    val activityCompose = "1.8.2"
+    val composeBom = "2023.03.00"
+    val testComposeJunit = "4.13.2"
+    val testComposeJunitExt = "1.1.5"
+    val testEspresso = "3.5.1"
+    val lifecycleViewmodelKtx = "2.6.2"
+    val navigationCompose = "2.7.6"
+}
